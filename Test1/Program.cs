bb4e06344b123be378bac797b9d4bc2889657b5c
Oleg <oleg.t@mail.ru>
2023-12-16 18:01:40 +0300

    HEAD Task1

---
+++ Test1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Test1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+            Console.ReadKey();
+        }
+    }
+}

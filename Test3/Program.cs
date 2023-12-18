6397d868c84160007eef3ea6447ae15c8225cf80
Oleg <oleg.t@mail.ru>
2023-12-18 11:21:28 +0300

    *HEAD part 2.7 task 1 & 2

---
+++ Test3/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Test3
+{
+    internal class Program
+    {
+        static string crbr = "\n";
+        
+        private string fullName;
+        private int age;
+        private string email;
+        private double scoreProgramming;
+        private double scoreMathemetics;
+        private double scorePhisics;
+
+        public void completeTestData()
+        {
+            this.age = 208;
+            this.fullName = "Ada Lovelace";
+            this.email = "unknown@unknown.unknown";
+            this.scoreMathemetics = Double.PositiveInfinity;
+            this.scorePhisics = Double.PositiveInfinity;
+            this.scoreProgramming = Double.PositiveInfinity;
+        }
+
+        private double getScoreAverage()
+        {
+            return (this.scoreProgramming + this.scorePhisics + this.scoreMathemetics) / 3;
+        }
+        
+        public static void Main(string[] args)
+        {
+            Program testPerson = new Program();
+            testPerson.completeTestData();
+            testPerson.delayedOutput();
+        }
+
+        public void delayedOutput()
+        {
+            Console.Write($"Fullname: {this.fullName}{crbr}");
+            Console.ReadKey();
+            Console.Write($"Age: {this.age}{crbr}");
+            Console.ReadKey();
+            Console.Write($"Email: {this.email}{crbr}");
+            Console.ReadKey();
+            Console.Write($"Score:{crbr}");
+            Console.ReadKey();
+            Console.Write($"  Programming: {this.scoreProgramming}{crbr}");
+            Console.ReadKey();
+            Console.Write($"  Mathematics: {this.scoreMathemetics}{crbr}");
+            Console.ReadKey();
+            Console.Write($"  Phisics: {this.scorePhisics}{crbr}");
+            Console.ReadKey();
+            Console.Write($"  Average: {this.getScoreAverage()}");
+            Console.ReadKey();
+        }
+
+        public string toString()
+        {
+            return $"Fullname: {this.fullName}{crbr}" +
+                   $"Age: {this.age}{crbr}" +
+                   $"Email: {this.email}{crbr}" +
+                   $"Score:{crbr}" +
+                   $"  Programming: {this.scoreProgramming}{crbr}" +
+                   $"  Mathematics: {this.scoreMathemetics}{crbr}" +
+                   $"  Phisics: {this.scorePhisics}{crbr}" +
+                   $"  Average: {this.getScoreAverage()}";
+        }
+    }
+}

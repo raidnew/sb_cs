4d91d97cfff165c2a9eeb40177ff57a1f363aac3
Oleg <oleg.t@mail.ru>
2023-12-21 13:34:28 +0300

    *HEAD Task 4.8

---
+++ Task4_8/GameOgLife.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task4_8
+{
+    public class GameOgLife
+    {
+        private uint width, height;
+        private bool[,] field;
+
+        private List<bool[,]> fieldHistory;
+
+        public GameOgLife(uint width = 10, uint height = 10)
+        {
+
+            this.width = width;
+            this.height = height;
+
+            this.field = new bool[height, width];
+            this.fieldHistory = new List<bool[,]>();
+
+            this.completeRandomField();
+
+            bool isLive = true;
+            int repeat = -1;
+            int delay = 500;
+            int step = 1;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine($"Iteration {step++}");
+                showField(this.field);
+                System.Threading.Thread.Sleep(delay);
+                isLive = nextGeneration();
+                repeat = checkOnRepeatFieldHistory(this.field);
+            } while (isLive && repeat < 0);
+
+            Console.Clear();
+            Console.WriteLine($"Iteration {step}");
+            showField(this.field);
+            if(!isLive) Console.WriteLine("All bio dead");
+            if(repeat >= 0) Console.WriteLine($"Field configuration repeat with generation {repeat}");
+        }
+
+        private void completeRandomField()
+        {
+            Random generator = new Random((int)DateTime.Now.Ticks);
+            for (int  i = 0; i < this.height; i++)
+            {
+                for (int j = 0; j < this.width; j++)
+                {
+                    this.field[i, j] = generator.Next(4) == 0 ? true : false;
+                }
+            }
+        }
+
+        public void showField(bool[,] field)
+        {
+            int index = 0;
+            foreach (bool value in field)
+            {
+                Console.Write(value ? "X" : "O");
+                index++;
+                if(index % this.width == 0) Console.WriteLine();
+            }
+        }
+
+        private uint getSummAroundCell(int x, int y)
+        {
+            uint summ = 0;
+            for (int  i = Math.Max(0, y - 1); i < Math.Min(height, y + 2);i++)
+            {
+                for (int j = Math.Max(0, x - 1); j < Math.Min(height, x + 2); j++)
+                {
+                    if ((x != j || y != i) && field[i, j]) summ++;
+                }
+            }
+            return summ;
+        }
+        
+        public bool nextGeneration()
+        {
+            bool[,] newField = new bool[height, width];
+            uint countAliveCells = 0;
+            
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (newField[i, j] = getNewCell(field[i, j], getSummAroundCell(j, i)))
+                    {
+                        countAliveCells++;
+                    }
+                }
+            }
+            
+            this.field = newField;
+            return countAliveCells > 0;
+        }
+
+        private int checkOnRepeatFieldHistory(bool[,] fieldForCheck)
+        {
+            for (int  i = 0; i < fieldHistory.Count; i++)
+            {
+                
+                if (compareFields(fieldHistory[i],fieldForCheck))
+                {
+                    return i;
+                }
+            }
+
+            fieldHistory.Add(fieldForCheck);
+            return -1;
+        }
+
+        private bool compareFields(bool[,] field1, bool[,] field2)
+        {
+            for (int i = 0;i<field1.GetLength(0);i++)
+            {
+                for (int j = 0; j < field1.GetLength(1); j++)
+                {
+                    if (field1[i, j] != field2[i, j]) return false;
+                }
+                    
+            }
+            return true;
+        }
+
+        private bool getNewCell(bool cellState, uint countCellAround)
+        {
+            if (countCellAround == 3)
+            {
+                return true;
+            }
+            else if (cellState && countCellAround == 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Task4_8/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Task4_8
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            /*
+            Matrix test = new Matrix(5,3);
+            test.completeRandomData();
+            Console.Write(test.toString());
+            Console.Write(test.summ());
+            Console.WriteLine();
+
+            Matrix test2 = new Matrix(5,3);
+            test2.completeRandomData();
+            Console.Write(test2.toString());
+
+            Console.WriteLine();
+            Matrix test3 = test + test2;
+            Console.Write(test3.toString());
+            */
+            GameOgLife test = new GameOgLife();
+        }
+    }
+
+    class Matrix
+    {
+        private uint width, height;
+        private int[,] data;
+        
+        public Matrix(uint width, uint height)
+        {
+            this.width = width;
+            this.height = height;
+            data = new int[height,width];
+        }
+
+        public static Matrix operator +(Matrix matrix1, Matrix matrix2)
+        {
+            if (matrix1.width != matrix2.width || matrix1.height != matrix2.height) return null;
+            Matrix result = new Matrix(matrix1.width, matrix1.height);
+            for (int i = 0; i<result.height;i++)
+            {
+                for (int j = 0;j<result.width; j++)
+                {
+                    result.data[i, j] = matrix1.getData(j, i) + matrix2.getData(j, i);
+                }
+            }
+            return result;
+        }
+
+        public int getData(int x, int y)
+        {
+            return this.data[y, x];
+        }
+        
+        public void completeRandomData()
+        {
+            Random rnd = new Random((int)DateTime.Now.Ticks);
+            for (int i = 0; i<this.height;i++)
+            {
+                for (int j = 0;j<this.width; j++)
+                {
+                    this.data[i,j] = rnd.Next(-100, 100);
+                }
+            }
+        }
+
+        public long summ()
+        {
+            long summ = 0;
+            foreach (int value in this.data)
+            {
+                summ += value;
+            }
+
+            return summ;
+        }
+
+        public string toString()
+        {
+            string retString = "";
+            int index = 0;
+            foreach (int value in this.data)
+            {
+                index++;
+                retString += $"{value}";
+                if (index % this.width == 0) retString += "\n";
+                else retString += " ";
+            }
+
+            return retString;
+        }
+    }
+}
